0714b122144a7c8d86192e0e803fadefecb377c2
Andrii833729 <spotiffay171717@gmail.com>
2025-11-03 09:59:52 +0200

    Add files via upload

---
+++ exportToHTML/script1.py
@@ -0,0 +1,8 @@
+print("ENTER PASSWORD")
+user_password = input ("Password: ")
+correct_password = "1111"
+if user_password == correct_password:
+    print("ACCESS GRANTED!")
+else:
+    print("ACCESS DENIED!")
+ANTI PLAGIAT
